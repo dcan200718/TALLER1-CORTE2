0f1fd1831eb14a69ac8bf338ccff7df4587e498f
Moon-collab1 <churches1229@gmail.com>
2026-04-08 16:38:20 +0000

    listo

--- lista.py
+++ lista.py
@@ -2,3 +2,42 @@ class lista:
     def __init__(self):
         self.head = None
 
+    def eliminar_primero(self):
+        if self.cabeza is None:
+            print("La lista está vacía. No hay nodo que eliminar.")
+            return
+        eliminado = self.cabeza.dato
+        self.cabeza = self.cabeza.siguiente
+        print(f"Nodo con valor '{eliminado}' eliminado del inicio.")
+
+    def eliminar_por_valor(self, dato):
+        
+        if self.cabeza is None:
+            print("La lista está vacía. No hay nodo que eliminar.")
+            return
+ 
+        if self.cabeza.dato == dato:
+            self.cabeza = self.cabeza.siguiente
+            print(f"Nodo con valor '{dato}' eliminado.")
+            return
+ 
+        anterior = self.cabeza
+        actual = self.cabeza.siguiente
+        while actual is not None:
+            if actual.dato == dato:
+                anterior.siguiente = actual.siguiente
+                print(f"Nodo con valor '{dato}' eliminado.")
+                return
+            anterior = actual
+            actual = actual.siguiente
+ 
+        print(f"El valor '{dato}' no fue encontrado en la lista.")
+
+def tamano(self):
+        contador = 0
+        actual = self.cabeza
+        while actual is not None:
+            contador += 1
+            actual = actual.siguiente
+        return contador
+ 
--- nodo.py
+++ nodo.py
@@ -3,3 +3,5 @@ class node:
       self.data = data
       self.next = next
 
+
+
